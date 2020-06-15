8dd3bb96aa45aee3229e0b859291e0181d302b80
fizreh <phaeza31@gmail.com>
2020-06-15 12:02:53 +0200

    update reset

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -10,27 +10,34 @@ export const AddTransaction = () => {
     const [amount,setAmount] = useState(0);
     const onSubmit =e=>{
       e.preventDefault();
+      if(amount=== 0 || amount ==='')
+      return alert('Please enter the amount. Please dont forget to add signs');
       const newTransaction = {
         id:Math.floor(Math.random()*100000000),
         text,
         amount:+amount,
 
       }
-      
+      if(newTransaction.text === '')
+      newTransaction.text = 'Anonymous';  
+      if(newTransaction.amount=== 0 || newTransaction.amount ==='')
+      return alert('Ooops! You forgot to add the amount.Please delete your entry and try agqain');
       
       addTransaction(newTransaction);
-    
+      setText('');
+      setAmount('');
             
       
     }
+   
     
     return (
         <div>
              <h3>Add new transaction</h3>
-      <form onSubmit = {onSubmit} id= 'create-course-form'>
+      <form onSubmit = {onSubmit} id= "create-course-form">
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input id='inputText' type="text" value={text} onChange={(e)=>setText(e.target.value)} placeholder="Enter text..." />
+          <input id='inputText' type="text" value={text} onChange={(e)=>setText(e.target.value)}  placeholder="Enter text..." />
         </div>
         <div className="form-control">
           <label htmlFor="amount"
@@ -39,7 +46,7 @@ export const AddTransaction = () => {
           >
           <input id='mainInput' type="number" value={amount} onChange={(e)=>setAmount(e.target.value)}  placeholder="Enter amount..." />
         </div>
-        <button className="btn" >Add transaction</button>
+        <button className="btn">Add transaction</button>
       </form>
         </div>
     )
